f0a43db8dc7770d4e651fc252ad426aa286f4813
Guanting <16443280+baconandchips@users.noreply.github.com>
2019-12-27 12:51:02 -0800

    Cleanup on files

--- contentScript.js
+++ contentScript.js
@@ -5,3 +5,14 @@
 	BEN324 CODES ABOVE THIS COMMENT
 	BACONANDCHIPS CODES BELOW THIS BUTTON
 */
+
+			var d = document;
+			var insideEmail = true;
+			if (insideEmail) {
+				console.log(document);
+				alert(document);
+
+				var emailText = document.querySelector('.adn.ads') // .children[1]; //.children[2].innerText;
+				//alert(emailText);
+				console.log(emailText);
+			}
